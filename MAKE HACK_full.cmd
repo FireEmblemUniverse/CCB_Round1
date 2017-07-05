9f2d45fce0dec3b5ff301df72d91b405d9e827da
Bovie Xiong <bovie.xiong@gmail.com>
2017-07-05 09:34:59 +0200

    add files

---
+++ MAKE HACK_full.cmd
@@ -0,0 +1,21 @@
+﻿cd %~dp0
+
+copy FE8_clean.gba CCB_Round1.gba
+
+cd "%~dp0Tables"
+
+echo: | (c2ea "%~dp0FE8_clean.gba")
+
+cd "%~dp0Text"
+
+echo: | (textprocess_v2 text_buildfile.txt)
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 "-output:%~dp0CCB_Round1.gba" "-input:%~dp0ROM Buildfile.event"
+
+cd "%~dp0ups"
+
+ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0CCB_Round1.gba" -o "%~dp0CCB_Round1.ups"
+
+pause
---
+++ MAKE HACK_quick.cmd
@@ -0,0 +1,9 @@
+﻿cd %~dp0
+
+copy FE8_clean.gba CCB_Round1.gba
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 "-output:%~dp0CCB_Round1.gba" "-input:%~dp0ROM Buildfile.event"
+
+pause
