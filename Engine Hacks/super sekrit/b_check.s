24229e04e9520e7c804182be41cedfb698d57815
Bovie Xiong <bovie.xiong@gmail.com>
2017-07-09 16:33:02 +0200

    fixed cursor speed

--- Engine Hacks/super sekrit/b_check.s
+++ Engine Hacks/super sekrit/b_check.s
@@ -1,7 +1,8 @@
 .thumb
-@hook at 801c8bc with jumptohack
-@also change 801c8b2 to ldrh r1,[r3,#0x8] to prevent key bounce
-
+@hook at 801c8c8 with jumptohack
+ldrb r0, [r3,#6] @prevent key bounce
+cmp r0, #0
+beq Normal
 ldr r0, =0x203aab0 @counter
 ldrb r1, [r0]
 cmp r1, #8
@@ -15,9 +16,16 @@ Reset:
 mov r1, #0
 strb r1, [r0]
 Normal:
-ldr r0, =0x202bcb0
-ldr r0, [r0, #0x20]
-ldr r1, =0x70007
-and r0, r1
-ldr r1, =0x801c8c5 @return location
+ldr r0, [r3,#0x10]
+ldr r1, =0x8015715
+bl goto_r1
+mov r0, #8
+ldr r1, =0x8015839
+bl goto_r1
+mov r0, #8
+ldr r1, =0x801588d
+bl goto_r1
+ldr r1, =0x801c8fd
+
+goto_r1:
 bx r1
