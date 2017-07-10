0d49530b1c0fe346af57d665634155ab0c24d85f
Bovie Xiong <bovie.xiong@gmail.com>
2017-07-10 21:05:58 +0200

    enemy canto was broken, tweaked skills

--- Engine Hacks/Skill System/Post Combat Skills/canto.s
+++ Engine Hacks/Skill System/Post Combat Skills/canto.s
@@ -62,17 +62,17 @@ mov	r1, #0x40	@set canto bit
 orr	r0, r1
 str	r0, [r4,#0x0C]
 
-@add unit to the AI list so enemies act twice
-ldr	r0,=#0x203AA03
-ldrb	r1, [r4,#0x0B]	@allegiance byte of the character we are checking
-AddAILoop:
-add	r0, #0x01
-ldrb	r2, [r0]
-cmp	r2, #0x00
-bne	AddAILoop
-strb	r1, [r0]
-add	r0, #0x01
-strb	r2, [r0]
+@ @add unit to the AI list so enemies act twice
+@ ldr	r0,=#0x203AA03
+@ ldrb	r1, [r4,#0x0B]	@allegiance byte of the character we are checking
+@ AddAILoop:
+@ add	r0, #0x01
+@ ldrb	r2, [r0]
+@ cmp	r2, #0x00
+@ bne	AddAILoop
+@ strb	r1, [r0]
+@ add	r0, #0x01
+@ strb	r2, [r0]
 
 End:
 pop	{r0}
