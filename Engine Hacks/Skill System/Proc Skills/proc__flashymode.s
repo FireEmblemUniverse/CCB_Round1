295e6bdecf0a2481993f62379c49b911538d2007
Bovie Xiong <bovie.xiong@gmail.com>
2017-07-12 20:43:08 +0200

    added palettes for void and tim, flashy mode bosses only

--- Engine Hacks/Skill System/Proc Skills/proc__flashymode.s
+++ Engine Hacks/Skill System/Proc Skills/proc__flashymode.s
@@ -11,6 +11,11 @@ mov r4, r0 @attacker
 mov r5, r1 @defender
 mov r6, r2 @battle buffer
 mov r7, r3 @battle data
+
+ldrb r0, [r4, #0xb] @don't activate against players
+cmp r0, #0x40
+blt End
+
 ldr     r0,[r2]           @r0 = battle buffer                @ 0802B40A 6800     
 lsl     r0,r0,#0xD                @ 0802B40C 0340     
 lsr     r0,r0,#0xD        @Without damage data                @ 0802B40E 0B40     
@@ -25,6 +30,15 @@ asr r0, #0x10
 ldrb r1, [r5, #0x13] @defender hp
 cmp r0, r1
 blt End @if it's not gonna kill
+
+@check defender for boss flag
+ldr r0, [r5] 
+add r0, #0x41
+ldrb r0, [r0] @ability 2
+mov r1, #0x80 @is boss?
+and r0, r1
+cmp r0, #0
+beq End @not a boss
 
 @if we kill, set the crit flag
 ldr     r2,[r6]    
