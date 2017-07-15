2a430fe53cf8da677d0b2a53742f411b0eec3c58
Bovie Xiong <bovie.xiong@gmail.com>
2017-07-15 21:23:10 +0200

    updated void mug and added boss quote for dk

---
+++ Events/playerphaseasmc.s
@@ -0,0 +1,18 @@
+@check if defender will die ASMC
+@playerphaseASMC
+.set slotlocation, 0x30004b8
+.thumb
+@check defender hp
+ldr r1, =(slotlocation + 4*0xc)
+ldr r0, =0x203a56c
+ldrb r0, [r0, #0x13]
+cmp r0, #0
+bgt False
+True:
+mov r0, #1
+b End
+False:
+mov r0, #0
+End:
+str r0, [r1]
+bx lr
